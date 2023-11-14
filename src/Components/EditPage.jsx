6300f8a97a9339db09f2d50c179987c413bdcb57
jameel914 <jameelibrahim107@gmail.com>
2023-11-13 22:34:17 -0500

    index page is done

--- src/Components/EditPage.jsx
+++ src/Components/EditPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 function EditPage() {
+  const API = import.meta.env.VITE_API_URL;
   return <div>EditPage</div>;
 }
 
--- src/Components/IndexPage.jsx
+++ src/Components/IndexPage.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function IndexPage() {
-  return <div>IndexPage</div>;
+  const API = import.meta.env.VITE_API_URL;
+  const [amount, setAmount] = useState([]);
+
+  useEffect(() => {
+    fetchIndex();
+  }, []);
+
+  async function fetchIndex() {
+    try {
+      let result = await axios.get(`${API}/transaction`);
+      console.log(result.data);
+      setAmount(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <th>Date</th>
+            <th>Category</th>
+            <th>Amount</th>
+          </tr>
+          {amount.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>
+                  <Link to={`/transaction/${item.id}`}>{item.date}</Link>
+                </td>
+                <td>
+                  <Link to={`/transaction/${item.id}`}>{item.category}</Link>
+                </td>
+                <td>
+                  <Link to={`/transaction/${item.id}`}>{item.amount}</Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default IndexPage;
