b212231b9a0d25d30feef2078dbaeb35ad48016a
jameel914 <jameelibrahim107@gmail.com>
2023-11-18 13:02:42 -0500

    fixed bug for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 import IndexPage from "./Components/IndexPage";
 import ShowPage from "./Components/ShowPage";
 import EditPage from "./Components/EditPage";
